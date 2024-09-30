9a0f250efa9e75191f22bf59199d540a17565b8e
Julia_Chu <z3211232001@gmail.com>
2024-09-30 15:38:04 -0700

    completed 04

--- ex04.js
+++ ex04.js
@@ -6,8 +6,16 @@ Create a function named instructorWithLongestName that will receive an array of 
 */
 
 const instructorWithLongestName = function (instructors) {
-  // Put your solution here
-};
+  let maxLengthIndex = 0
+  let maxLength = 0
+  instructors.forEach((element, index) => {
+    if (element.name.length > maxLength) {
+      maxLength = element.name.length
+      maxLengthIndex = index
+    }
+  })
+  return instructors[maxLengthIndex]
+}
 
 console.log(
   instructorWithLongestName([
