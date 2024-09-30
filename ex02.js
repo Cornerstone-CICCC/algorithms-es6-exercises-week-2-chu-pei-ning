f8d0c277e2b7cfc8584d3d08da58cba13f31e51d
Julia_Chu <z3211232001@gmail.com>
2024-09-30 13:40:43 -0700

    completed 02

--- ex02.js
+++ ex02.js
@@ -3,12 +3,24 @@ We'll be adding only the numbers in the array which match the given condition.
 
 Instruction
 Create a function named conditionalSum that will be given an array of numbers and a condition, in this case odd or even. Given this condition, add up only the values which match that condition. If no values match the condition, return 0.
-
+const notExpired = products.filter(function(product) {
+    return product.expiry >= 2024;
 */
 
 const conditionalSum = function (values, condition) {
-  // Your code here
-};
+    if (condition === "even"){
+        const checkEven = values.filter( even => even % 2 === 0)
+        const sum = checkEven.reduce((total, currentElement) => total + currentElement, 0 )
+        return sum
+    } else if (condition === "odd"){
+        const checkOdd= values.filter( odd => odd % 2 !== 0)
+        const sum = checkOdd.reduce((total, currentElement) => total + currentElement, 0 )
+        return sum
+    } else {
+        return "I can't check even or odd."
+    }
+    return sum
+}
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even")); // 6
 console.log(conditionalSum([1, 2, 3, 4, 5], "odd")); // 9
