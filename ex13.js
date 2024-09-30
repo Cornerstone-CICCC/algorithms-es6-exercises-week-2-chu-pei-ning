94a5f300d89f1d94f04a398e372ed92849693c78
Julia_Chu <z3211232001@gmail.com>
2024-09-30 13:53:15 -0700

    completed 13

--- ex13.js
+++ ex13.js
@@ -9,8 +9,31 @@ Create a function named talkingCalendar that takes in a date string with the for
 */
 
 const talkingCalendar = function (date) {
-  // Your code here
-};
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+  let dateArr = date.split('/')
+  let strMonth = months[parseInt(dateArr[1]) - 1]
+  let strDay = dateToDate(dateArr[2])
+
+  return `${strMonth} ${strDay}, ${dateArr[0]}`
+}; // December 2nd, 2017
+
+function dateToDate(numDate) {
+  switch (numDate){
+    case 1: case 21: case 31:
+      numDate += "st";
+      break;
+    case 2: case 22:
+      numDate += "nd";
+      break;
+    case 3: case 23: 
+      numDate += "rd";
+      break;
+    default:
+      numDate += "th";
+      break;
+  }
+}
 
 console.log(talkingCalendar("2017/12/02")); // December 2nd, 2017
 console.log(talkingCalendar("2007/11/11")); // November 11th, 2007
